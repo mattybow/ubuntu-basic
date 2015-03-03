523fa451298bd3f00301ff3ebd6668e93803aabb
Matt Bow <mattybow@gmail.com>
2015-03-03 01:56:38 -0600

    rearranged order

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 sudo apt-get install -y git
 sudo apt-get install -y curl
-curl https://raw.githubusercontent.com/creationix/nvm/v0.23.3/install.sh | bash
+sudo apt-get install -y zsh
 
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sudo zsh
+sudo chsh -s `which zsh` ubuntu
+
+curl https://raw.githubusercontent.com/creationix/nvm/v0.23.3/install.sh | sudo zsh
 source ~/.nvm/nvm.sh
 nvm install stable
 nvm use stable
 
-sudo apt-get install -y zsh
-curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sudo zsh
-sudo chsh -s `which zsh` ubuntu
 sudo shutdown -r 0
